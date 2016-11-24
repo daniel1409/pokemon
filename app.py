f5c3ab6d0a6e7235c7d16ca636f457f1f1bc9666
Matheus Candido <Matheus@iMac-de-r13.local>
2016-11-24 13:44:36 -0200

    adiciona ultimo arquivo

--- app.py
+++ app.py
@@ -5,28 +5,35 @@ app = Flask('pokemon')
 
 engine = create_engine('postgresql://localhost/pokemon')
 
+
+
+
+
+
+
+
 @app.route('/pokemons/types')
 def types():
-    return jsonify(types=[
-        ['Bug', 69],
-        ['Dragon', 32],
-        ['Fighting', 27],
-        ['Psychic', 57],
-        ['Steel', 27],
-        ['Ice', 24],
-        ['Rock', 44],
-        ['Fire', 52],
-        ['Grass', 70],
-        ['Ground', 32],
-        ['Electric', 44],
-        ['Dark', 31],
-        ['Flying', 4],
-        ['Normal', 98],
-        ['Ghost', 32],
-        ['Water', 112],
-        ['Poison', 28],
-        ['Fairy', 17],
-    ])
+  return jsonify(types=[
+    ['Bug', 69],
+    ['Dragon', 32],
+    ['Fighting', 27],
+    ['Psychic', 57],
+    ['Steel', 27],
+    ['Ice', 24],
+    ['Rock', 44],
+    ['Fire', 52],
+    ['Grass', 70],
+    ['Ground', 32],
+    ['Electric', 44],
+    ['Dark', 31],
+    ['Flying', 4],
+    ['Normal', 98],
+    ['Ghost', 32],
+    ['Water', 112],
+    ['Poison', 28],
+    ['Fairy', 17],
+   ])
 
 @app.route('/')
 def index():
